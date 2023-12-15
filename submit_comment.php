b291aaa5bf1b7bc5f48985be2f40859284562dd4
Ahtesham Ibne Mostafa <ahtesham.ibne.mostafa@g.bracu.ac.bd>
2023-12-16 03:01:53 +0600

    Update submit_comment.php

--- submit_comment.php
+++ submit_comment.php
@@ -2,19 +2,22 @@
 include("includes/config.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+   
     $post_id = $_POST['post_id'];
     $comment_content = $_POST['comment_content'];
 
-
     $insertComment = $con->prepare("INSERT INTO comment (post_id, username, content) VALUES (?, ?, ?)");
+
     $username = 'anonymous';
     $insertComment->bind_param("iss", $post_id, $username, $comment_content);
 
     if ($insertComment->execute()) {
         header("Location: forum.php"); 
+        exit();
     } else {
-        echo "Error: " . $con->error; 
+        echo "Error: " . $con->error;
+    }
+} else {
     echo "Access denied";
 }
 $con->close();
